744a1c0d86379052b525878158a6bee324cbd20a
Debora-k <debora0104k@gmail.com>
2024-10-22 23:28:47 -0600

    login with email feature

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -6,7 +6,14 @@ import { initialCart } from "../cart/cartSlice";
 
 export const loginWithEmail = createAsyncThunk(
   "user/loginWithEmail",
-  async ({ email, password }, { rejectWithValue }) => {}
+  async ({ email, password }, { rejectWithValue }) => {
+    try{
+      const response = await api.post("/auth/login", {email,password});
+      return response.data;
+    }catch(error){
+      return rejectWithValue(error.error);
+    }
+  }
 );
 
 export const loginWithGoogle = createAsyncThunk(
@@ -65,6 +72,18 @@ const userSlice = createSlice({
       state.loading=false;
       state.registrationError=action.payload;
     })
+    .addCase(loginWithEmail.pending,(state)=> {
+      state.loading=true;
+    })
+    .addCase(loginWithEmail.fulfilled,(state,action)=>{
+      state.loading=false;
+      state.user=action.payload.user;
+      state.loginError=null;
+    })
+    .addCase(loginWithEmail.rejected,(state,action)=>{
+      state.loading=false;
+      state.loginError=action.payload;
+    }) 
   },
 });
 export const { clearErrors } = userSlice.actions;
